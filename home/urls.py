f0ee2a964cff3bed2e502750e513ba1f4a819178
DULCE JASMIN VILLALOBOS PEREZ <dulce.villalobos17@tectijuana.edu.mx>
2021-02-26 23:41:55 -0800

    PW

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 
-from django.urls import path
+from django.contrib import admin
+
+from django.urls import path, include
 
 app_name = "home"
 
 urlpatterns = [
     path('index', views.index.as_view(), name="index"),
 ]
+
+
